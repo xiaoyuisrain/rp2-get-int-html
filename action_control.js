24a5efa94a0a29f71f7b09a5beb41af3b53ea7e2
xiaoyuisrain <35065433+xiaoyuisrain@users.noreply.github.com>
2020-09-15 01:34:50 +0200

    Respondent types out entire text.

--- action_control.js
+++ action_control.js
@@ -10,12 +10,6 @@ function aft_int0(){
       // Display step after paraphrase is provided.
       document.getElementById("pp_notprovided").setAttribute("class", "bef_proceed");
       document.getElementById("pp_provided").setAttribute("class", "aft_proceed");
-      // Copy reference text.
-      document.getElementById("display_pp").innerHTML = document.getElementById("ref").innerHTML;
-      // Replace target word.
-      var word_to_replace = document.getElementById("display_pp").getElementsByClassName("tw")[0];
-      word_to_replace.setAttribute("class", "int");
-      word_to_replace.innerHTML = value_int0; 
     }
   } else {
     // Hide the next step.
