31b8f5e68c074130e5b965564080e2a42a013fc5
xiaoyuisrain <35065433+xiaoyuisrain@users.noreply.github.com>
2020-09-17 11:05:27 +0200

    Get original text excl. target word.

--- action_control.js
+++ action_control.js
@@ -1,6 +1,13 @@
 function aft_int0(){
+    var text0_html = document.getElementById("text0").innerHTML;
+    var warning = document.getElementById("compare_warn");
+    warning.innerHTML = text0_html;
+    warning.getElementsByClassName("tw")[0].outerHTML = "";
+    text0_excl = warning.innerText; // original string excl. tw
+    // TODO: get tokens in original text excl. tw
   // Get input int0
   var value_int0 = document.getElementById("int0").value;
+    // TODO: compare original and pp
   if (value_int0.length) {
     if (value_int0 == document.getElementById("value_na").innerText) {
       // Display step after respondent chooses to give up the task.
