c894b26a2a8b29838df9b27fcc9f62f4b082cb0f
xiaoyuisrain <35065433+xiaoyuisrain@users.noreply.github.com>
2020-09-17 11:12:44 +0200

    Reformatted code: 2 spaces.

--- action_control.js
+++ action_control.js
@@ -1,10 +1,10 @@
 function aft_int0(){
-    var text0_html = document.getElementById("text0").innerHTML;
-    var warning = document.getElementById("compare_warn");
-    warning.innerHTML = text0_html;
-    warning.getElementsByClassName("tw")[0].outerHTML = "";
-    text0_excl = warning.innerText; // original string excl. tw
-    // TODO: get tokens in original text excl. tw
+  var text0_html = document.getElementById("text0").innerHTML;
+  var warning = document.getElementById("compare_warn");
+  warning.innerHTML = text0_html;
+  warning.getElementsByClassName("tw")[0].outerHTML = "";
+  text0_excl = warning.innerText; // original string excl. tw
+  // TODO: get tokens in original text excl. tw
   // Get input int0
   var value_int0 = document.getElementById("int0").value;
     // TODO: compare original and pp
