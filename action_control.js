c07e46f90a97e35d789c82f98b30cdddc7669bda
xiaoyuisrain <35065433+xiaoyuisrain@users.noreply.github.com>
2020-09-17 15:21:53 +0200

    Compare tokens0 and tokens1.

--- action_control.js
+++ action_control.js
@@ -10,24 +10,50 @@ function formatPuncs(string) {
 }
 
 function aft_int0() {
-  // Get original text excluding target word and punctuations
-  var text0_html = document.getElementById("text0").innerHTML;
-  var warning = document.getElementById("compare_warn");
-  warning.innerHTML = text0_html;
-  warning.getElementsByClassName("tw")[0].outerHTML = "";
-  text0_excl = formatPuncs(warning.innerText);
-  // Get non-target tokens in original text
-  tokens0 = text0_excl.split(" "); 
   // Get input int0
   var value_int0 = document.getElementById("int0").value;
   if (value_int0.length) {
     if (value_int0 == document.getElementById("value_na").innerText) {
-      // TODO: compare original and pp
-      tokens1 = formatPuncs(value_int0).split(" ");
       // Display step after respondent chooses to give up the task.
       document.getElementById("pp_provided").setAttribute("class", "bef_proceed");
       document.getElementById("pp_notprovided").setAttribute("class", "aft_proceed");
     } else {
+      // Get original text excluding target word and punctuations
+      var text0_html = document.getElementById("text0").innerHTML;
+      var warning = document.getElementById("compare_warn");
+      warning.setAttribute("class", "bef_proceed");
+      warning.innerHTML = text0_html;
+      warning.getElementsByClassName("tw")[0].outerHTML = "";
+      text0_excl = formatPuncs(warning.innerText);
+      // Get non-target tokens in original text
+      tokens0 = text0_excl.split(" "); 
+      // Split provided paraphrase into tokens
+      tokens1 = formatPuncs(value_int0).split(" ");
+      console.log(tokens0);
+      console.log(tokens1);
+      tokens1.forEach(function map2Original(value) {
+        /* Compare the focus token with the token
+         * at the beginning of tokens0,
+         * and remove the latter from tokens0 if the two are the same.
+         * */
+        beginner = tokens0.shift();
+        if (value != beginner) {
+          tokens0.unshift(beginner); 
+        }
+      }); 
+      console.log(tokens0);
+      // Display warning if there are tokens not removed from tokens0
+      if (tokens0.length) {
+        warning.innerHTML = "Non-target words might have been modified." +
+          " Please double-check and correct your paraphrase if applicable." + 
+          " After correcting your paraphrase, please click 'Proceed'." +
+          " If you are certain that your paraphrase is correct," +
+          " please ignore this message.";
+        warning.setAttribute("class", "aft_proceed wrong");
+      } else {
+        warning.innerHTML = "";
+        warning.setAttribute("class", "bef_proceed");
+      }
       // Display step after paraphrase is provided.
       document.getElementById("pp_notprovided").setAttribute("class", "bef_proceed");
       document.getElementById("pp_provided").setAttribute("class", "aft_proceed");
